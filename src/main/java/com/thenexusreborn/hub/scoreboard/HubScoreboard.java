1aa1ecfbd9a1e032bb76a53af359cdc82c72e0df
Firestar311 <slaterandrew0398@gmail.com>
2022-11-23 04:36:05 -0600

    Rewrote some of the backend for scoreboards to make it easier to create them in code

--- src/main/java/com/thenexusreborn/hub/scoreboard/HubScoreboard.java
+++ src/main/java/com/thenexusreborn/hub/scoreboard/HubScoreboard.java
@@ -1,22 +1,14 @@
 package com.thenexusreborn.hub.scoreboard;
 
 import com.thenexusreborn.api.NexusAPI;
-import com.thenexusreborn.api.helper.StringHelper;
 import com.thenexusreborn.api.player.*;
-import com.thenexusreborn.api.scoreboard.NexusScoreboard;
-import com.thenexusreborn.api.scoreboard.wrapper.*;
+import com.thenexusreborn.api.scoreboard.*;
 import com.thenexusreborn.nexuscore.scoreboard.SpigotScoreboardView;
 import com.thenexusreborn.nexuscore.util.MCUtils;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 
-import java.util.*;
-
 public class HubScoreboard extends SpigotScoreboardView {
-    
-    private final String rankLabelName = "rankLabel", rankValueName = "rankValue", blank1Name = "blank1", balanceLabelName = "balanceLabel", 
-    creditsValueName = "creditsValue", nexitesValueName = "nexitesValue", blank2Name = "blank2", playersLabelName = "playersLabel", playersValueName = "playersValue", 
-    blank3Name = "blank3", serverLabelName = "serverLabel", serverValueName = "serverValue";
     
     public HubScoreboard(NexusScoreboard scoreboard) {
         super(scoreboard, "lobby", MCUtils.color("&5&lTHE NEXUS"));
@@ -24,50 +16,35 @@ public class HubScoreboard extends SpigotScoreboardView {
     
     @Override
     public void registerTeams() {
-        NexusPlayer player = this.scoreboard.getPlayer();
-        IScoreboard scoreboard = this.scoreboard.getScoreboard();
-        ITeam rankLabel = scoreboard.registerNewTeam(rankLabelName);
-        addEntry(objective, rankLabel, MCUtils.color("&6&lRANK:"), 15);
-        
-        ITeam rankValue = scoreboard.registerNewTeam(rankValueName);
-        rankValue.setPrefix(MCUtils.color(player.getRanks().get().getColor() + StringHelper.capitalizeEveryWord(player.getRanks().get().name())));
-        addEntry(objective, rankValue, ChatColor.GREEN.toString(), 14);
-        
-        ITeam blank1 = scoreboard.registerNewTeam(blank1Name);
-        addEntry(objective, blank1, ChatColor.GOLD.toString(), 13);
-        
-        ITeam balanceLabel = scoreboard.registerNewTeam(balanceLabelName);
-        addEntry(objective, balanceLabel, MCUtils.color("&6&lBALANCE:"), 12);
-        
-        ITeam creditsValue = scoreboard.registerNewTeam(creditsValueName);
-        creditsValue.setPrefix(MCUtils.color("&fCredits: "));
-        double credits = player.getStats().getValue("credits").getAsDouble();
-        creditsValue.setSuffix(MCUtils.color("&3" + formatBalance(credits)));
-        addEntry(objective, creditsValue, ChatColor.DARK_RED.toString(), 11);
-        
-        ITeam nexitesValue = scoreboard.registerNewTeam(nexitesValueName);
-        nexitesValue.setPrefix(MCUtils.color("&fNexites: "));
-        nexitesValue.setSuffix(MCUtils.color("&9" + formatBalance(player.getStats().getValue("nexites").getAsDouble())));
-        addEntry(objective, nexitesValue, ChatColor.AQUA.toString(), 10);
-    
-        ITeam blank2 = scoreboard.registerNewTeam(blank2Name);
-        addEntry(objective, blank1, ChatColor.BLACK.toString(), 9);
-        
-        ITeam playersLabel = scoreboard.registerNewTeam(playersLabelName);
-        addEntry(objective, playersLabel, MCUtils.color("&6&lPLAYERS:"), 8);
-        
-        ITeam playersValue = scoreboard.registerNewTeam(playersValueName);
-        playersValue.setPrefix(MCUtils.color("&f" + Bukkit.getServer().getOnlinePlayers().size() + "/" + Bukkit.getServer().getMaxPlayers()));
-        addEntry(objective, playersValue, ChatColor.YELLOW.toString(), 7);
-        
-        ITeam blank3 = scoreboard.registerNewTeam(blank3Name);
-        addEntry(objective, blank3, ChatColor.DARK_BLUE.toString(), 6);
-        
-        ITeam serverLabel = scoreboard.registerNewTeam(serverLabelName);
-        addEntry(objective, serverLabel, MCUtils.color("&6&lSERVER:"), 5);
-    
-        ITeam serverValue = scoreboard.registerNewTeam(serverValueName);
-        addEntry(objective, serverValue, MCUtils.color("&f" + NexusAPI.getApi().getServerManager().getCurrentServer().getName()), 4);
+        createTeam(new TeamBuilder("rankLabel").entry("&6&lRANK:").score(15));
+        createTeam(new TeamBuilder("rankValue").entry(ChatColor.GREEN.toString()).score(14).valueUpdater((player, team) -> {
+            Rank rank = player.getRanks().get();
+            team.setPrefix(rank.getColor() + rank.name());
+        }));
+        createTeam(new TeamBuilder("blank1").entry(ChatColor.GOLD.toString()).score(13));
+        createTeam(new TeamBuilder("balanceLabel").entry("&6&lBALANCE:").score(12));
+        createTeam(new TeamBuilder("creditsValue").entry("&fCredits: &3").score(11).valueUpdater((player, team) -> team.setSuffix(formatBalance(player.getStats().getValue("credits").getAsDouble()))));
+        createTeam(new TeamBuilder("nexites").entry("&fNexites: &9").score(10).valueUpdater((player, team) -> team.setSuffix(formatBalance(player.getStats().getValue("nexites").getAsDouble()))));
+        createTeam(new TeamBuilder("blank2").entry(ChatColor.BLACK.toString()).score(9));
+        createTeam(new TeamBuilder("playersLabel").entry("&6&lPLAYERS").score(8));
+        createTeam(new TeamBuilder("playersValue").entry(ChatColor.YELLOW.toString()).score(7).valueUpdater((player, team) -> {
+            int onlinePlayers = 0;
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                NexusPlayer onp = NexusAPI.getApi().getPlayerManager().getNexusPlayer(onlinePlayer.getUniqueId());
+                if (onp == null) continue;
+                if (onp.getToggles().getValue("incognito") || onp.getToggles().getValue("vanish")) {
+                    if (player.getRanks().get().ordinal() <= Rank.HELPER.ordinal()) {
+                        onlinePlayers++;
+                    }
+                } else {
+                    onlinePlayers++;
+                }
+            }
+            team.setPrefix(MCUtils.color("&f" + onlinePlayers + "/" + Bukkit.getServer().getMaxPlayers()));
+        }));
+        createTeam(new TeamBuilder("blank3").entry(ChatColor.DARK_BLUE.toString()).score(6));
+        createTeam(new TeamBuilder("serverLabel").entry("&6&lSERVER:").score(5));
+        createTeam(new TeamBuilder("serverValue").entry("&f" + NexusAPI.getApi().getServerManager().getCurrentServer().getName()).score(4));  
     }
     
     private String formatBalance(double balance) {
@@ -75,39 +52,5 @@ public class HubScoreboard extends SpigotScoreboardView {
             return MCUtils.formatNumber(balance);
         }
         return MCUtils.formatNumber(balance / 1000) + "k";
-    }
-    
-    @Override
-    public void update() {
-        NexusPlayer player = this.scoreboard.getPlayer();
-        IScoreboard scoreboard = this.scoreboard.getScoreboard();
-        String rankName;
-        if (player.getRanks().get() == Rank.MEMBER) {
-            rankName = "Member";
-        } else {
-            rankName = player.getRanks().get().name();
-        }
-        scoreboard.getTeam(this.rankValueName).setPrefix(MCUtils.color(player.getRanks().get().getColor() + rankName));
-        scoreboard.getTeam(this.creditsValueName).setSuffix(MCUtils.color("&3" + formatBalance(player.getStats().getValue("credits").getAsDouble())));
-        scoreboard.getTeam(this.nexitesValueName).setSuffix(MCUtils.color("&9" + formatBalance(player.getStats().getValue("nexites").getAsDouble())));
-        int onlinePlayers = 0;
-        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-            NexusPlayer onp = NexusAPI.getApi().getPlayerManager().getNexusPlayer(onlinePlayer.getUniqueId());
-            if (onp == null) continue;
-            if (onp.getToggles().getValue("incognito") || onp.getToggles().getValue("vanish")) {
-                if (player.getRanks().get().ordinal() <= Rank.HELPER.ordinal()) {
-                    onlinePlayers++;
-                }
-            } else {
-                onlinePlayers++;
-            }
-        }
-        scoreboard.getTeam(this.playersValueName).setPrefix(MCUtils.color("&f" + onlinePlayers + "/" + Bukkit.getServer().getMaxPlayers()));
-    }
-    
-    @Override
-    public List<String> getTeams() {
-        return Arrays.asList(rankLabelName, rankValueName, blank1Name, balanceLabelName, creditsValueName, nexitesValueName, blank2Name, playersLabelName, playersValueName, 
-                blank3Name, serverLabelName, serverValueName);
     }
 }
