2811f61599c5161448d86b0c8a6d8deafcc22b16
Firestar311 <slaterandrew0398@gmail.com>
2024-12-30 18:24:03 -0600

    Fixed an issue with /spawn being able to be used outside of the hub

--- src/main/java/com/thenexusreborn/hub/cmds/SpawnCmd.java
+++ src/main/java/com/thenexusreborn/hub/cmds/SpawnCmd.java
@@ -2,8 +2,10 @@ package com.thenexusreborn.hub.cmds;
 
 import com.stardevllc.starcore.color.ColorHandler;
 import com.thenexusreborn.hub.NexusHub;
-import com.thenexusreborn.nexuscore.util.*;
-import org.bukkit.command.*;
+import com.thenexusreborn.nexuscore.util.MsgType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SpawnCmd implements CommandExecutor {
@@ -18,7 +20,12 @@ public class SpawnCmd implements CommandExecutor {
             sender.sendMessage(ColorHandler.getInstance().color("&cOnly players can use that command."));
             return true;
         }
-    
+        
+        if (!plugin.getHubServer().getPlayers().contains(player.getUniqueId())) {
+            sender.sendMessage(ColorHandler.getInstance().color("&cYou cannot use that command right now"));
+            return true;
+        }
+
         player.teleport(plugin.getSpawn());
         player.sendMessage(ColorHandler.getInstance().color(MsgType.INFO + "Teleported to the spawn."));
         return true;
